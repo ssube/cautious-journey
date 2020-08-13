37ea3e5fe3410a9c54f576bd339ca3885b4779fe
ssube <seansube@gmail.com>
2020-08-12 20:01:48 -0500

    feat(sync): check actual difference between labels

--- src/sync.ts
+++ src/sync.ts
@@ -1,5 +1,7 @@
-import { FlagLabel, getLabelNames, StateLabel } from './labels';
-import { Remote } from './remote';
+import { doesExist, mustExist, NotFoundError } from '@apextoaster/js-utils';
+
+import { FlagLabel, getLabelNames, StateLabel, StateValue } from './labels';
+import { Remote, LabelUpdate } from './remote';
 
 // TODO: turn this back on/remove the disable pragma
 /* eslint-disable no-console */
@@ -23,6 +25,23 @@ export async function syncIssues(options: SyncOptions): Promise<unknown> {
   return undefined;
 }
 
+function findSyncLabel(name: string, flags: Array<FlagLabel>, states: Array<StateLabel>): FlagLabel | StateValue {
+  const flagIndex = flags.find((it) => it.name === name);
+  if (doesExist(flagIndex)) {
+    return flagIndex;
+  }
+
+  const stateIndex = states.find((it) => name.startsWith(it.name));
+  if (doesExist(stateIndex)) {
+    const valueIndex = stateIndex.values.find((it) => `${stateIndex.name}/${it.name}` === name);
+    if (doesExist(valueIndex)) {
+      return valueIndex;
+    }
+  }
+
+  throw new NotFoundError('label not found');
+}
+
 export async function syncLabels(options: SyncOptions): Promise<unknown> {
   const labels = await options.remote.listLabels({
     project: options.project,
@@ -30,14 +49,19 @@ export async function syncLabels(options: SyncOptions): Promise<unknown> {
 
   const existingLabels = new Set(labels.map((l) => l.name));
   const expectedLabels = getLabelNames(options.flags, options.states);
+  const allLabels = new Set([...expectedLabels, ...existingLabels]);
 
-  for (const label of labels) {
-    const exists = existingLabels.has(label.name);
-    const expected = expectedLabels.has(label.name);
+  for (const label of allLabels) {
+    const exists = existingLabels.has(label);
+    const expected = expectedLabels.has(label);
+    console.log('label:', label, exists, expected);
 
     if (exists) {
       if (expected) {
-        console.log('update label:', label);
+        const existingData = mustExist(labels.find((l) => l.name === label));
+        const expectedData = findSyncLabel(label, options.flags, options.states);
+
+        await updateLabel(existingData, expectedData);
       } else {
         console.log('remove label:', label);
       }
@@ -51,4 +75,14 @@ export async function syncLabels(options: SyncOptions): Promise<unknown> {
   }
 
   return undefined;
+}
+
+export async function updateLabel(existingData: LabelUpdate, expectedData: FlagLabel | StateValue): Promise<void> {
+  const dirty =
+    existingData.color !== expectedData.color ||
+    existingData.desc !== expectedData.desc;
+
+  if (dirty) {
+    console.log('update label:', existingData, expectedData);
+  }
 }
