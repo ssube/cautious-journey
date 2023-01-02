6afa62ecbe5b1ca8a376f2be6325b77d6ad88d5f
Sean Sube <seansube@gmail.com>
2023-01-01 21:41:54 -0600

    fix(build): add codeclimate target

--- config/project.mk
+++ config/project.mk
@@ -8,3 +8,7 @@ run-graph:
 
 run-version:
 	node --require esm ./out/index.js --version
+
+upload-climate:
+	cc-test-reporter format-coverage -t lcov -o $(TARGET_PATH)/coverage/codeclimate.json -p $(ROOT_PATH) $(TARGET_PATH)/coverage/lcov.info
+	cc-test-reporter upload-coverage --debug -i $(TARGET_PATH)/coverage/codeclimate.json -r "$(shell echo "${CODECLIMATE_SECRET}" | base64 -d)"
